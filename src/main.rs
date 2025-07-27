2eb608d50b17d160dcc6da50745eca7e3aaafdc3
Cyncrovee <86919717+Cyncrovee@users.noreply.github.com>
2025-07-27 22:24:32 +0100

    refactor: move insert input handling to it's own file

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::io::stdout;
 
 use crossterm::{
     cursor,
-    event::{self, Event, KeyCode, KeyEventKind, KeyModifiers},
+    event::{self, Event, KeyEventKind},
     execute,
 };
 use ropey::RopeBuilder;
 use util::{
-    check_target_col, move_down, move_left, move_right, move_up, new_line, print_content, statusline, to_col, to_row, up_line
+    statusline, to_col, to_row
 };
 
 mod util;
@@ -53,64 +53,14 @@ impl AugeliteState {
                 if key.kind == KeyEventKind::Press {
                     match self.mode {
                         Mode::Ovr => {
-                            modes::overview::overview_input(key, self);
+                            if !modes::overview::overview_input(key, self) {
+                                break
+                            }
                         }
-                        Mode::Ins => match key.code {
-                            KeyCode::Char(c) => {
-                                if c == 'q' && key.modifiers == KeyModifiers::CONTROL {
-                                    execute!(stdout(), crossterm::terminal::LeaveAlternateScreen)
-                                        .unwrap();
-                                    break;
-                                } else {
-                                    let text = self.buffer.clone().finish();
-                                    let mut text = text.to_string();
-                                    text.insert(self.cursor_char, c);
-                                    self.buffer = RopeBuilder::new();
-                                    self.buffer.append(text.as_str());
-                                    print_content(self.buffer.clone().finish(), false).unwrap();
-                                    move_right();
-                                    self.target_col = cursor::position().unwrap().0.into();
-                                }
+                        Mode::Ins => {
+                            if !modes::insert::insert_input(key, self) {
+                                break
                             }
-                            KeyCode::Left => {
-                                cursor_movement::cursor_left(self);
-                            }
-                            KeyCode::Right => {
-                                cursor_movement::cursor_right(self);
-                            }
-                            KeyCode::Up => {
-                                cursor_movement::cursor_up(self);
-                            }
-                            KeyCode::Down => {
-                                cursor_movement::cursor_down(self);
-                            }
-                            KeyCode::Enter => {
-                                self.buffer.append("\n");
-                                new_line();
-                            }
-                            KeyCode::Backspace => {
-                                if self.cursor_pos != (0, 0) {
-                                    let mut text = self.buffer.clone().finish();
-                                    text.remove(self.cursor_char - 1..self.cursor_char);
-                                    self.buffer = RopeBuilder::new();
-                                    self.buffer.append(text.to_string().as_str());
-                                    if cursor::position().unwrap().0 != 0 {
-                                        move_left();
-                                    } else {
-                                        up_line();
-                                        to_col(
-                                            text.line(cursor::position().unwrap().1.into())
-                                                .len_chars()
-                                                .try_into()
-                                                .unwrap(),
-                                        );
-                                    }
-                                    self.target_col = self.cursor_pos.0.into();
-                                    print_content(self.buffer.clone().finish(), true).unwrap();
-                                }
-                            }
-                            KeyCode::Esc => self.mode = Mode::Ovr,
-                            _ => {}
                         },
                     }
                     self.cursor_pos = cursor::position().unwrap();
---
+++ src/modes/insert.rs
@@ -0,0 +1,67 @@
+use std::io::stdout;
+
+use crossterm::{cursor, event::{KeyCode, KeyEvent, KeyModifiers}, execute};
+use ropey::RopeBuilder;
+
+use crate::{cursor_movement, util::{move_left, move_right, new_line, print_content, to_col, up_line}, AugeliteState, Mode};
+
+pub fn insert_input(key: KeyEvent, main_struct: &mut AugeliteState) -> bool {
+    match key.code {
+        KeyCode::Char(c) => {
+            if c == 'q' && key.modifiers == KeyModifiers::CONTROL {
+                execute!(stdout(), crossterm::terminal::LeaveAlternateScreen)
+                    .unwrap();
+                return false;
+            } else {
+                let text = main_struct.buffer.clone().finish();
+                let mut text = text.to_string();
+                text.insert(main_struct.cursor_char, c);
+                main_struct.buffer = RopeBuilder::new();
+                main_struct.buffer.append(text.as_str());
+                print_content(main_struct.buffer.clone().finish(), false).unwrap();
+                move_right();
+                main_struct.target_col = cursor::position().unwrap().0.into();
+            }
+        }
+        KeyCode::Left => {
+            cursor_movement::cursor_left(main_struct);
+        }
+        KeyCode::Right => {
+            cursor_movement::cursor_right(main_struct);
+        }
+        KeyCode::Up => {
+            cursor_movement::cursor_up(main_struct);
+        }
+        KeyCode::Down => {
+            cursor_movement::cursor_down(main_struct);
+        }
+        KeyCode::Enter => {
+            main_struct.buffer.append("\n");
+            new_line();
+        }
+        KeyCode::Backspace => {
+            if main_struct.cursor_pos != (0, 0) {
+                let mut text = main_struct.buffer.clone().finish();
+                text.remove(main_struct.cursor_char - 1..main_struct.cursor_char);
+                main_struct.buffer = RopeBuilder::new();
+                main_struct.buffer.append(text.to_string().as_str());
+                if cursor::position().unwrap().0 != 0 {
+                    move_left();
+                } else {
+                    up_line();
+                    to_col(
+                        text.line(cursor::position().unwrap().1.into())
+                            .len_chars()
+                            .try_into()
+                            .unwrap(),
+                    );
+                }
+                main_struct.target_col = main_struct.cursor_pos.0.into();
+                print_content(main_struct.buffer.clone().finish(), true).unwrap();
+            }
+        }
+        KeyCode::Esc => main_struct.mode = Mode::Ovr,
+        _ => {}
+    }
+    true
+}
